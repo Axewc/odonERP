8ecf0c589ab1ea2febe948a62ce8e3b0a65c4521
Axel <axew@ciencias.unam.mx>
2025-03-26 21:47:01 -0600

    🔧 Configurar carga de variables de entorno y actualizar conexión a la base de datos

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
 from flask import Flask, render_template
-from models import db, Paciente  # Asegúrate de importar Paciente
+from models import db, Paciente
+from dotenv import load_dotenv
+import os
 
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:210622@localhost/erp_clinico'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# app.config.from_object('config.Config')
+# Configurar la URI de la base de datos usando las variables de entorno
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # Ruta corregida
+    return render_template('index.html')
 
 @app.route('/pacientes')
 def mostrar_pacientes():
     pacientes = Paciente.query.all()
-    return render_template('pacientes.html', pacientes=pacientes)  # Ruta corregida
+    return render_template('pacientes.html', pacientes=pacientes)
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Asegúrate de que la base de datos está inicializada
+        db.create_all()
     app.run(debug=True)
